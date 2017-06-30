d7eebf4bab69071c20933f9539e6259e997c6b67
bibliomist <bibliomist@mail.ru>
2017-06-30 17:49:16 +0300

    03

--- app/src/main/java/com/a279/siemens/mydiary/MainActivity.java
+++ app/src/main/java/com/a279/siemens/mydiary/MainActivity.java
@@ -1,13 +1,49 @@
 package com.a279.siemens.mydiary;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        initNavigationDraver();
+    }
+
+    private void initNavigationDraver() {
+        DrawerLayout dl = (DrawerLayout) findViewById(R.id.drawerlayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dl, toolbar, R.string.nav_open, R.string.nav_close);
+        dl.setDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.mAdd:
+                        Toast.makeText(MainActivity.this, "Add", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mSettings:
+                        Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu_toolbar);
+        toolbar.setTitle("MyDiary");
     }
 }
